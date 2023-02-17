2ae9ee42fb08c3b0dd93f78bcef00c7677569102
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2023-02-17 10:54:05 +0900

    User model を実装

--- follow_app/models.py
+++ follow_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    # id は書かなくても自動で作成されるので、name だけ書く
+    # CharField(max_length=20) が、 VARCHAR(20) に相当する
+    name = models.CharField(max_length=20)
